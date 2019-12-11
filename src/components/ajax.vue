ec3dcdec0bfbcf841c00985b0d31baca30af91e2
Zhanyu <1635259245@qq.com>
2019-12-11 09:48:46 +0800

    ajax_

--- src/components/ajax.vue
+++ src/components/ajax.vue
@@ -9,7 +9,14 @@
     name: "ajax",
     methods: {
       btn(){
+        // eslint-disable-next-line no-undef
+        ajax("https://www.12306.cn/index/",function (xhr) {
+          alert(xhr.responseText);
+          // eslint-disable-next-line no-unused-vars
+        },function (xhr) {
+          alert("请求失败");
 
+        })
       }
   }
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import ajax from './ajax/ajax'
 Vue.config.productionTip = false
 
+// Vue.use(ajax);
+
 new Vue({
+  ajax,
   render: h => h(App),
 }).$mount('#app')
